75e52981692ec4c29ac65169fa79d03a29f3d2cb
ifzyy <johnsonnifemi8@gmail.com>
2022-01-07 22:04:50 +0000

    index.html

--- index.js
+++ index.js
@@ -1,86 +1,86 @@
-function computerPlay () {
-    let options = [ 'rock', 'paper', 'scissors']
-    return options[Math.floor(Math.random() * options.length)]
-        
+const computerChoiceDisplay = document.getElementById('computer-choice')
+const userChoiceDisplay = document.getElementById('user-choice')
+const resultDisplay = document.getElementById('result')
+const possibleChoices = document.querySelectorAll('button')
+const scoreBoard= document.getElementById('scores')
+let userChoice
+let computerChoice
+let result
+
+possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {
+  userChoice = e.target.id
+  userChoiceDisplay.innerHTML = userChoice
+  generateComputerChoice()
+  getResult()
+}))
+
+function generateComputerChoice() {
+  const randomNumber = Math.floor(Math.random() * 3) + 1 
+  
+  if (randomNumber === 1) {
+    computerChoice = 'rock'
+  }
+  if (randomNumber === 2) {
+    computerChoice = 'scissors'
+  }
+  if (randomNumber === 3) {
+    computerChoice = 'paper'
+  }
+  computerChoiceDisplay.innerHTML = computerChoice
 }
-    let playerScore = 0
-    let compScore = 0
+
+function getResult() {
+  if (computerChoice === userChoice) {
+    result = 'its a draw!'
+  }
+  if (computerChoice === 'rock' && userChoice === "paper") {
+    let win =  `congrats You win! ${userChoice} beats ${computerChoice}`;
+    result = win
+    playerScore++
+    
+  }
+  if (computerChoice === 'rock' && userChoice === "scissors") {
+    let lose =  ` You lose yo!  ${computerChoice} beats ${userChoice}`;
+    result= lose
+    compScore++
+  }
+  if (computerChoice === 'paper' && userChoice === "scissors") {
+    let win =  `congrats You win! ${userChoice} beats ${computerChoice}`;
+    result = win
+    playerScore++
+  }
+  if (computerChoice === 'paper' && userChoice === "rock") {
+    let lose =  ` You lose yo!  ${computerChoice} beats ${userChoice}`;
+    result= lose
+    compScore++
+  }
+  if (computerChoice === 'scissors' && userChoice === "rock") {
+    let win =  `congrats You win! ${userChoice} beats ${computerChoice}`;
+    result = win
+    playerScore++
+  }
+  if (computerChoice === 'scissors' && userChoice === "paper") {
+    let lose =  ` You lose yo!  ${computerChoice} beats ${userChoice}`;
+    result= lose
+    compScore++
+  }
+  resultDisplay.innerHTML = result
+  scoreBoard.innerHTML= scoreUpdate()
+}
+let playerScore = 0
+let compScore = 0
 
 
 function scoreUpdate() {
-    if (playerScore === 5) {
-        playerScore && compScore
-        return "You've won!!!!";
+if (playerScore === 5) {
+    playerScore && compScore
+    return "You've won!!!!";
+
+} else if (compScore === 5) {
+    playerScore && compScore
+    return "You lose motherfucker!!!!"
     
-    } else if (compScore === 5) {
-        playerScore && compScore
-        return "You lose!!!!"
-        
-    } else { 
-        return `${playerScore} - ${compScore}`;
-    }
-}
-   
-
-function playRound(playerSelection, computerSelection){
-  
-
-    if (playerSelection === 'rock' && computerSelection === 'scissors'|| 
-        playerSelection === 'paper' && computerSelection === 'rock' ||
-        playerSelection === 'scissors' && computerSelection === 'paper') {
-            let win =  `congrats You win! ${playerSelection} beats ${computerSelection}`;
-            alert(`${win} (${++playerScore}-${compScore})`)
-         return scoreUpdate()
-         
-                 
-    } else if (playerSelection === 'scissors' && computerSelection === 'rock' ||
-    playerSelection === 'rock' && computerSelection === 'paper' ||
-    playerSelection === 'paper' && computerSelection === 'scissors' ) {
-        let lose =  ` You lose yo!  ${computerSelection} beats ${playerSelection}`;
-        alert(`${lose} (${playerScore} - ${++compScore})`)
-        return scoreUpdate()
-        
-        
-     
-} else{
-    let tie = `Tie! ${computerSelection} cannot win against ${playerSelection}`;
-    alert(tie)
-    return scoreUpdate()
+} else { 
+    return `${playerScore} - ${compScore}`;
 }
 }
-
-
-let playgame = confirm("shall we play a game of rock paper scissors")
-if(playgame){
-    //play
-    function game(playerSelection) {
-        playerSelection = prompt('choose rock, paper, or scissors')
-     let computerSelection = computerPlay();
-             alert(computerSelection)
-             alert(playRound(playerSelection, computerSelection))
-
-     }
-}
-else{
-   alert("Ok maybe next time") 
-}
-        
-
-for (let i = 0; i < 5; i++) {
-    game()
-    
-}
-let playagain=alert("Reload the page if you'd like to play again")
-   
-
-
-
-
-
-
-
-
-
-
-
-
